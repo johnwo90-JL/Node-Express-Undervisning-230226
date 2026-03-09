659dfabe37977050702fbe1c9c43d58cc43de581
johnwo90-JL <john-age@jobloop.no>
2026-03-09 12:07:57 +0100

    Chore: Cleanup (to be) unused files

--- src/controllers/user-controller/getAllUsers.js
+++
@@ -1,9 +0,0 @@
-import { User } from "../../models/user.model.js";
-
-
-
-export const getAllUsers = async (req, res) => {
-    console.log("[Handler] Request ID:", req.requestId);
-
-    res.status(200).json(await User.findAll());
-};
