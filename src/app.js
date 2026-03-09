25f37e9f0d832729a4ebce23ced8ce090bfe8557
johnwo90-JL <john-age@jobloop.no>
2026-03-09 12:10:36 +0100

    Chore: Update `app.js` to accomodate changes

--- src/app.js
+++ src/app.js
@@ -1,36 +1,55 @@
 import express from "express"
-import userRouter from "./routes/userRouter.js"
 import { syncDatabase, testConnection } from "./db/sqlite.db.js";
 import cookieParser from "cookie-parser";
 import { config } from "./config/env.config.js";
+import { createAuthController } from "./controllers/auth.controller.js";
+import { createUsersController } from "./controllers/users.controller.js";
+import { createAuthMiddlewares } from "./middlewares/use-auth.middleware.js";
+import { createAuthRouter } from "./routes/auth.router.js";
+import { createUsersRouter } from "./routes/users.router.js";
 import { createServices } from "./services/index.js";
+import "./models/session-token.model.js";
 
 await testConnection();
 await syncDatabase();
 
 const app = express();
 const services = createServices();
-const cookieService = services.cookie;
+const authController = createAuthController({
+    authService: services.auth,
+    cookieService: services.cookie,
+    env: config.env,
+});
+const usersController = createUsersController({
+    userService: services.user,
+    logger: services.logger,
+});
+const authMiddlewares = createAuthMiddlewares({
+    authService: services.auth,
+});
+const authRouter = createAuthRouter({
+    authController,
+    authMiddlewares,
+});
+const usersRouter = createUsersRouter({
+    usersController,
+    authMiddlewares,
+});
 
 // LEGGE INN JSON PARSIN (BODY)
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(cookieParser(config.auth.cookie.secret));
 
-
-app.use('/users', userRouter);
-
-// Vårt aller første ENDEPUNKT!
-app.get('/', (req, res) => {
-    res.send("Hello world!");
+app.get("/", (req, res) => {
+    res.status(200).json({
+        success: true,
+        message: "OK",
+    });
 });
 
-// Vårt aller første ENDEPUNKT!
-app.get('/cookie', (req, res) => {
-    const cookie = cookieService.createSignedCookie("test", "cookie!", { secure: true });
-    res.cookie(cookie.name, cookie.value, cookie.options);
-    res.send();
-});
+app.use("/auth", authRouter);
+app.use("/users", usersRouter);
 
 // "Endestasjon" -> "Catch-all"
 app.use((err, req, res, next) => {
